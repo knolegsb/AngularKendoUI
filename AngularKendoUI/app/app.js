777d297213403ec1e4f5bf63187123569514bccd
Sean <seon2you@hotmail.com>
2016-08-14 17:48:49 -0700

    Add project files.

---
+++ AngularKendoUI/app/app.js
@@ -0,0 +1,12 @@
+﻿(function(){
+    'use strict';
+
+    var app = angular.module('ngKendoApp', ['ngRoute', 'kendo.directives']);
+
+    app.config(['$routeProvider', function($routeProvider){
+        $routeProvider
+            .otherwise({ redirectTo: '/' });
+    }
+        
+    ]);
+})();
