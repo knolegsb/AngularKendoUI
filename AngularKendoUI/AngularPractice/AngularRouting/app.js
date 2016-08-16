8869881bebcd51fc8fc6ae1747d07c4c679dc9d8
Sean <seon2you@hotmail.com>
2016-08-15 21:53:58 -0700

    sorting with table header

---
+++ AngularKendoUI/AngularPractice/AngularRouting/app.js
@@ -0,0 +1,34 @@
+﻿var app = angular.module("routingApp", ["ngRoute"])
+            .config(function ($routeProvider, $locationProvider) {
+                $routeProvider
+                    .when("/home", {
+                        templateUrl: "templates/home.html",
+                        controller: "homeController"
+                    })
+                    .when("/courses", {
+                        templateUrl: "templates/courses.html",
+                        controller: "coursesController"
+                    })
+                    .when("/students", {
+                        templateUrl: "templates/students.html",
+                        controller: "studentsController"
+                    })
+                    .otherwise({
+                        redirectTo: '/home',
+                        controller: "homeController"
+                    });
+                //$locationProvider.Html5Mode(true);
+            })
+
+.controller("homeController", function ($scope) {
+    $scope.message = "Home Page";
+})
+.controller("coursesController", function ($scope) {
+    $scope.courses = ["C#", "VB.NET", "SQL Server", "ASP.NET"];
+})
+.controller("studentsController", function ($scope, $http) {
+    $http.get("StudentService.asmx/GetAllStudents")
+        .then(function (response) {
+            $scope.students = response.data;
+        });
+});
---
+++ AngularKendoUI/AngularPractice/AngularSorting/script.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module("sortingModule", [])
+                .controller("sortingController", function ($scope) {
+                    var employees = [
+                        { Name: "Ben", DateOfBirth: new Date("November 23, 1980"), Gender: "Male", Salary: 55000 },
+                        { Name: "Sara", DateOfBirth: new Date("May 05, 1970"), Gender: "Female", Salary: 68000 },
+                        { Name: "Mark", DateOfBirth: new Date("August 15, 1974"), Gender: "Male", Salary: 57000 },
+                        { Name: "Pam", DateOfBirth: new Date("October 27, 1979"), Gender: "Female", Salary: 53000 },
+                        { Name: "Todd", DateOfBirth: new Date("December 30, 1983"), Gender: "Male", Salary: 60000 },
+                    ];
+
+                    $scope.employees = employees;
+                    $scope.sortColumn = "Name";
+                    $scope.reverseSort = false;
+
+                    $scope.sortData = function (column) {
+                        $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+                        $scope.sortColumn = column;
+                    }
+
+                    $scope.getSortClass = function (column) {
+                        if ($scope.sortColumn == column) {
+                            return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
+                        }
+
+                        return '';
+                    }
+                });
